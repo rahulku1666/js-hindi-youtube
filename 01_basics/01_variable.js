b5a913eeefac8ab37ca19ab9ab02e6efae94aab2
Rahul kumar <rahulku7122000@gmail.com>
2024-08-14 16:58:26 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rahul@google.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert ( 3 + 3 ) //we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) //code readability should be high
+
+console.log("Rahul")     
+
+
+let name = "Rahul"
+let age = 23
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigInt
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof"Rahul"); // undefined
+console.log(typeof null); // object
