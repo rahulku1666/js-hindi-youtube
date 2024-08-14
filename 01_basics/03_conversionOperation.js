7d94a42ef68981b99737a9059ae4487b02567ba2
Rahul kumar <rahulku7122000@gmail.com>
2024-08-14 17:34:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Rahul"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "Rahul"
+
+let booleanIsLoggedIn = boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rahul" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
